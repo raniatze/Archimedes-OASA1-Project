7d51322c76cff07d4de30aae6b164574ec2d6f98
Ninostorm <nvamvakaris@gmail.com>
2023-04-04 00:01:24 +0300

    db initial commit with data downloading

---
+++ inserter.py
@@ -0,0 +1,25 @@
+import csv
+import argparse
+from pymongo import MongoClient
+
+"""
+CSV using ; to separate values
+"""
+
+# Parse command-line arguments
+parser = argparse.ArgumentParser()
+parser.add_argument('-f', '--filename', type=str, required=True, help='CSV file to import')
+parser.add_argument('-c', '--collection', type=str, required=True, help='MongoDB collection to insert into')
+parser.add_argument('-s', '--separator', type=str, required=False, default=';', help='value separator')
+args = parser.parse_args()
+
+
+client = MongoClient('mongodb://localhost:27017/')
+db = client['OASA1']
+collection = db[args.collection]
+
+
+with open(args.filename, 'r') as ake:
+    reader = csv.DictReader(ake, delimiter=args.separator)
+    for row in reader:
+        collection.insert_one(row)
