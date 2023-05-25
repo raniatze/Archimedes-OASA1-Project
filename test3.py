0702e7661e42b2e6bfa0710a3d05bbac5b57a535
Christina Ourania Tze <64203505+raniatze@users.noreply.github.com>
2023-05-25 17:47:10 +0300

    updated first try on LSTM dataset

--- test3.py
+++ test3.py
@@ -21,6 +21,25 @@ def get_previous_stops(m, index, df):
     print("Previous stop orders")       
     print(result_df)
     return result_df
+    
+def get_previous_days(n, index, df):
+    result_df = pd.DataFrame()
+    # Find current_day_of_year and current_year
+    current_stop_id, current_stop_order = df.iloc[index]['Stop_id'], df.iloc[index]['Stop_order']
+    filtered_df = df[(df['Stop_id'] == current_stop_id) & (df['Stop_order'] == current_stop_order)]
+    filtered_df = filtered_df.reset_index()
+    print("Current Row")
+    print(pd.DataFrame(df.iloc[index]).transpose())
+    print("Filtered Df")
+    print(filtered_df)
+    new_index = filtered_df.index[filtered_df['index'] == index].tolist()[0]
+    filtered_df.drop(['index'], axis=1, inplace=True)
+    if new_index >= n:
+       for i in range(n):
+         previous_row = filtered_df.iloc[new_index - (i+1)]
+         result_df = pd.concat([result_df, pd.DataFrame([previous_row])], ignore_index=True)
+
+    return result_df
 
 def create_input_sequences(line_descr_df, m, n):
     input_sequences = [] # X_train
@@ -40,13 +59,21 @@ def create_input_sequences(line_descr_df, m, n):
             # Iterate over the group, starting from sequence_length index
             for i in range(m, group_length):
                 # Get the previous stops for the current instance
-                previous_stops = get_previous_stops(m, i, group_sorted)
+                #previous_stops = get_previous_stops(m, i, group_sorted)
                 
-                if len(previous_stops) != m:
-                   continue
+                #if len(previous_stops) != m:
+                   #continue
 
                 # Get the previous days' stops for the current instance
-                #previous_days = get_previous_days(data, group['Day_of_year'].iloc[i], sequence_length)
+                previous_days = get_previous_days(n, i, group_sorted)
+                
+                if len(previous_days) != n:
+                   print("No previous days")
+                   continue
+                else:
+                   previous_days = previous_days.sort_values(['Year', 'Day_of_year'], ascending = [True, True])
+                   previous_days = previous_days.reset_index(drop=True)
+                   print(previous_days)
 
                 # Combine the previous stops and previous days' stops
                 #inputs = previous_stops + previous_days
@@ -79,29 +106,6 @@ def filter_line_descr(line_descr):
     # Return the query result
     return list(result)
 
-def get_previous_days(n, stop_id, stop_order, day_of_year, df):
-    # Filter a specific stop_id and stop_order
-    filtered_df = df[(df['Stop_id'] == stop_id) & (df['Stop_order'] == int(stop_order))]
-
-    # Sort filtered dataframe
-    sorted_df = filtered_df.sort_values(by='Day_of_year', ascending=True).reset_index(drop=True)
-
-    # Find the index of the row with the specific day of year
-    index = sorted_df[sorted_df['Day_of_year'] == int(day_of_year)].index[0]
-
-    # Use the index to slice the dataframe and retrieve the previous m rows
-    previous_days_df = sorted_df.loc[index-n:index-1]
-    result_length = len(previous_days_df)
-
-    # Fill missing values with NaN
-    if result_length < n:
-       missing_rows = n - result_length
-       missing_data = pd.DataFrame(np.nan, index=np.arange(missing_rows), columns=previous_days_df.columns)
-       previous_days_df = pd.concat([previous_days_df, missing_data]).reset_index(drop=True)
-
-    return previous_days_df
-
-
 # Example usage
 m = 3 # previous stop_orders
 n = 2 # previous days
@@ -113,10 +117,10 @@ sched = "1900-01-01 05:50:00"
 day_of_year = "96"
 year = "2021"
 
-
 filtered_data = filter_line_descr(line_descr)
 line_descr_df = pd.DataFrame(filtered_data)
 line_descr_df.drop(['_id'], axis=1, inplace=True)
+line_descr_df['Stop_id'] = line_descr_df['Stop_id'].astype(int)
 line_descr_df['Stop_order'] = line_descr_df['Stop_order'].astype(int)
 line_descr_df['Day_of_year'] = line_descr_df['Day_of_year'].astype(int)
 line_descr_df['Minute_of_day'] = line_descr_df['Minute_of_day'].astype(int)
