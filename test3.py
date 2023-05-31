6a058580c1574eba548a948900a36b9312c38241
Christina Ourania Tze <64203505+raniatze@users.noreply.github.com>
2023-05-31 17:49:04 +0300

    updated test3.py

--- test3.py
+++ test3.py
@@ -1,3 +1,5 @@
+import os
+import csv
 import pandas as pd
 import numpy as np
 from pymongo import MongoClient
@@ -31,16 +33,20 @@ def get_previous_stops(m, index, df):
 
     return result_df
 
-def get_previous_days(n, index, df):
+def get_previous_days(n, index, df, stops_dict):
     result_df = pd.DataFrame()
     # Find current_day_of_year and current_year
     current_stop_id, current_stop_order = df.iloc[index]['Stop_id'], df.iloc[index]['Stop_order']
-    filtered_df = df[(df['Stop_id'] == current_stop_id) & (df['Stop_order'] == current_stop_order)]
+
+    if (current_stop_id, current_stop_order) not in stops_dict:
+       stops_dict[(current_stop_id, current_stop_order)] = df[(df['Stop_id'] == current_stop_id) & (df['Stop_order'] == current_stop_order)]
+
+    filtered_df = stops_dict[(current_stop_id, current_stop_order)]
     filtered_df = filtered_df.reset_index()
     #print("Current Row")
     #print(pd.DataFrame(df.iloc[index]).transpose())
-    print("Filtered Df")
-    print(filtered_df)
+    #print("Filtered Df")
+    #print(filtered_df)
     new_index = filtered_df.index[filtered_df['index'] == index].tolist()[0]
     filtered_df.drop(['index'], axis=1, inplace=True)
     if new_index >= n:
@@ -50,69 +56,80 @@ def get_previous_days(n, index, df):
 
     return result_df
 
-def create_input_sequences(line_descr_df, m, n):
-    input_sequences = [] # X_train
-    target_values = [] # Y_train
+def create_input_sequences(num_line_descr, line_descr_df, m, n):
 
-    # Group the data by Day_of_year
-    grouped_data = line_descr_df.groupby(['Direction', 'Sched'])
+    folder_path = 'LSTM_Dataset_' + num_line_descr
+    os.makedirs(folder_path)
 
-    for name, group in grouped_data:
-        print("################################################### NEW GROUP #####################################")
-        print(name)
-        group_sorted = group.sort_values(['Year', 'Day_of_year', 'Stop_order'], ascending=[True, True, True])
-        group_sorted = group_sorted.reset_index(drop=True)
-        # Get the sequence length for the current Day_of_year group
-        group_length = len(group_sorted)
-        if group_length > m:
-        
-            # Iterate over the group, starting from sequence_length index
-            for i in range(m, group_length):
-            
-                # Get the previous stops and days for the current instance
-                previous_stops, previous_days = get_previous_stops(m, i, group_sorted), get_previous_days(n, i, group_sorted)
-                num_previous_stops, num_previous_days = len(previous_stops), len(previous_days)
-                if num_previous_stops == 0 or num_previous_days == 0:
-                   print("Skip current row")
-                   continue
-                else:
-                   previous_stops = previous_stops.sort_values(['Stop_order'], ascending = True).reset_index(drop=True)
-                   previous_days = previous_days.sort_values(['Year', 'Day_of_year'], ascending = [True, True]).reset_index(drop=True)
-                   num_missing_stops, num_missing_days = m - num_previous_stops, n - num_previous_days
-                   
-                   if num_missing_stops != 0:
-                      new_previous_stops = copy_rows(previous_stops, num_missing_stops)
-                   else:
-                      new_previous_stops = previous_stops
-                   
-                   if num_missing_days != 0:
-                      new_previous_days = copy_rows(previous_days, num_missing_days)
-                   else:
-                      new_previous_days = previous_days
-                   print("Previous stops")
-                   print(new_previous_stops)
-                   print("Previous days")
-                   print(new_previous_days)
-                   
-                # Combine the previous stops and previous days' stops
-                inputs = pd.concat([new_previous_stops, new_previous_days], axis=0).reset_index(drop=True)
-                inputs.drop(['Sched', 'Year'], axis=1, inplace=True)
-                print("Input")
-                print(inputs)
+    with open(os.path.join(folder_path, 'inputs.csv'), 'w') as input_file, open(os.path.join(folder_path, 'targets.csv'), 'w', newline='') as target_file:
 
-                # Get the target value (T_pa_in_veh) for the current instance
-                target = group_sorted.iloc[i]['T_pa_in_veh']
+         csv_input_writer, csv_target_writer = csv.writer(input_file, delimiter=','), csv.writer(target_file)
 
-                input_sequences.append(inputs)
-                target_values.append(target)
-        else:
-            continue
+         # Write header row in output_file
+         csv_input_writer.writerow(['Line_descr', 'Direction', 'Stop_id', 'Stop_order', 'Minute_of_day', 'Day_of_month', 'Day_of_week', 'Week_of_year', 'Day_of_year', 'Is_holiday', 'Temperature', 'Precipitation', 'T_pa_in_veh'])
+         csv_target_writer.writerow(['T_pa_in_veh'])
 
-    # Convert the input_sequences and target_values to numpy arrays
-    input_sequences = np.array(input_sequences)
-    target_values = np.array(target_values)
+         # Group the data by Day_of_year
+         grouped_data = line_descr_df.groupby(['Direction', 'Sched'])
 
-    return input_sequences, target_values
+         for name, group in grouped_data:
+            print("################################################### NEW GROUP #####################################")
+            print(name)
+            stops_dict = {} # Dictionary with (stop_id, stop_order) keys and filtered_df values
+            group_sorted = group.sort_values(['Year', 'Day_of_year', 'Stop_order'], ascending=[True, True, True])
+            group_sorted = group_sorted.reset_index(drop=True)
+            # Get the sequence length for the current Day_of_year group
+            group_length = len(group_sorted)
+            if group_length > m:
+
+                # Iterate over the group, starting from sequence_length index
+                for i in range(m, group_length):
+
+                    # Get the previous stops and days for the current instance
+                    previous_stops, previous_days = get_previous_stops(m, i, group_sorted), get_previous_days(n, i, group_sorted, stops_dict)
+                    num_previous_stops, num_previous_days = len(previous_stops), len(previous_days)
+                    if num_previous_stops == 0 or num_previous_days == 0:
+                       print("Skip current row")
+                       continue
+                    else:
+                       previous_stops = previous_stops.sort_values(['Stop_order'], ascending = True).reset_index(drop=True)
+                       previous_days = previous_days.sort_values(['Year', 'Day_of_year'], ascending = [True, True]).reset_index(drop=True)
+                       num_missing_stops, num_missing_days = m - num_previous_stops, n - num_previous_days
+
+                       if num_missing_stops != 0:
+                          new_previous_stops = copy_rows(previous_stops, num_missing_stops)
+                       else:
+                          new_previous_stops = previous_stops
+
+                       if num_missing_days != 0:
+                          new_previous_days = copy_rows(previous_days, num_missing_days)
+                       else:
+                          new_previous_days = previous_days
+                       #print("Previous stops")
+                       #print(new_previous_stops)
+                       #print("Previous days")
+                       #print(new_previous_days)
+
+                    # Combine the previous stops and previous days' stops
+                    inputs = pd.concat([new_previous_stops, new_previous_days], axis=0).reset_index(drop=True)
+                    inputs.drop(['Sched', 'Year'], axis=1, inplace=True)
+                    print("Input")
+                    print(inputs)
+                    if inputs.shape != (5,13):
+                       print(" WRONG ")
+                       print(inputs)
+                       x = input()
+
+                    # Get the target value (T_pa_in_veh) for the current instance
+                    target = group_sorted.iloc[i]['T_pa_in_veh']
+                    print("Target")
+                    print(target)
+
+                    csv_input_writer.writerows(inputs.values)
+                    csv_target_writer.writerow([target])
+            else:
+                continue
+    return
 
 def filter_line_descr(line_descr):
     # Find the matching documents
@@ -131,15 +148,16 @@ def filter_line_descr(line_descr):
 # Example usage
 m = 3 # previous stop_orders
 n = 2 # previous days
-line_descr = "1"
+num_line_descr = "1"
 
-filtered_data = filter_line_descr(line_descr)
+filtered_data = filter_line_descr(num_line_descr)
 line_descr_df = pd.DataFrame(filtered_data)
 line_descr_df.drop(['_id'], axis=1, inplace=True)
 line_descr_df['Stop_id'] = line_descr_df['Stop_id'].astype(int)
 line_descr_df['Stop_order'] = line_descr_df['Stop_order'].astype(int)
 line_descr_df['Day_of_year'] = line_descr_df['Day_of_year'].astype(int)
 line_descr_df['Minute_of_day'] = line_descr_df['Minute_of_day'].astype(int)
+line_descr_df['T_pa_in_veh'] = line_descr_df['T_pa_in_veh'].astype(int)
 line_descr_df['Year'] = line_descr_df['Year'].astype(int)
 
-X, y = create_input_sequences(line_descr_df, m, n)
+create_input_sequences(num_line_descr, line_descr_df, m, n)
