dae8bdfa2e47aa0d81fb7e4a43801cb08f5c5141
Christina Ourania Tze <64203505+raniatze@users.noreply.github.com>
2023-06-14 18:03:16 +0300

    updated test4.py

--- test4.py
+++ test4.py
@@ -19,22 +19,25 @@ class LSTM_model:
         self.model = self.build_model()
 
     def build_model(self):
-        model = Sequential()
-        model.add(LSTM(units=64, input_shape=self.input_shape, return_sequences=True))
-        model.add(LSTM(units=32, return_sequences=False))
-        model.add(Dense(units=self.output_shape, activation='linear'))
-        model.compile(loss='mean_squared_error', optimizer='adam')
-        return model
+        self.model = Sequential()
+        self.model.add(LSTM(units=64, input_shape=self.input_shape, return_sequences=True))
+        self.model.add(LSTM(units=32, return_sequences=False))
+        self.model.add(Dense(units=self.output_shape, activation='linear'))
+        self.model.compile(loss='mean_squared_error', optimizer='adam')
 
-    def train(self, X_train, y_train, X_test, y_test, num_epochs, batch_size):
-        # update with your_path
-        save_fname = os.path.join('your_path', '%s-e%s.h5' % (dt.datetime.now().strftime('%d%m%Y-%H%M%S'), str(num_epochs)))
+    def train(self, X_train, y_train, epochs, batch_size, validation_data):
+        
+        # Define the directory path for saving the checkpoints
+        checkpoint_dir = os.path.join(os.getcwd(), 'Checkpoints', num_line_descr)
 
-        callbacks = [EarlyStopping(monitor='val_loss', patience=50),
-                     ModelCheckpoint(filepath=save_fname, monitor='val_loss',save_best_only=True)]
-        history = self.model.fit(X_train, y_train, epochs=num_epochs, batch_size=batch_size, verbose=1,
-                             validation_data=(X_test, y_test), callbacks=callbacks)
-        self.model.save(save_fname)
+        # Create the directory if it doesn't exist
+        os.makedirs(checkpoint_dir)
+    
+        checkpoint_callback = ModelCheckpoint(os.path.join(checkpoint_dir, 'model_checkpoint_{epoch:02d}.h5'), save_freq='epoch', save_best_only=False)
+        early_stopping_callback = EarlyStopping(monitor='val_loss', patience=10)
+
+        history = self.model.fit(X_train, y_train, epochs=epochs, batch_size=batch_size, verbose=1,
+                             validation_data=validation_data, callbacks=[checkpoint_callback, early_stopping_callback])
         return history
 
     def evaluate(self, X_test, y_test):
@@ -53,6 +56,9 @@ dataset_folder_path = "LSTM_Dataset_" + num_line_descr
 
 input_sequence = pd.read_csv(os.path.join(dataset_folder_path, 'inputs.csv'), delimiter=',')
 target_input_sequence = pd.read_csv(os.path.join(dataset_folder_path, 'targets.csv'), delimiter=',')
+print(input_sequence.shape)
+print(target_input_sequence.shape)
+x = input()
 
 target_sequence = target_input_sequence['T_pa_in_veh']
 
@@ -85,45 +91,50 @@ for i in range(0, input_sequence.shape[0], look_back):
 # Perform train-test split
 #X_train, X_test, y_train, y_test = train_test_split(X_2d, y_1d, test_size=0.2, random_state=42)
 
-# Define the train-test split ratio
+# Define the train-validation-test split ratios
+validation_size = 0.1
 test_size = 0.2
 
-# Calculate the number of samples for testing
+# Calculate the number of samples for validation and testing
+num_validation_samples = int(validation_size * num_samples)
 num_test_samples = int(test_size * num_samples)
 
 # Shuffle the data indices
 indices = np.arange(num_samples)
 np.random.shuffle(indices)
 
-# Split the indices into train and test sets
-train_indices = indices[:-num_test_samples]
+# Split the indices into train, validation, and test sets
+train_indices = indices[:-(num_validation_samples + num_test_samples)]
+val_indices = indices[-(num_validation_samples + num_test_samples):-num_test_samples]
 test_indices = indices[-num_test_samples:]
 
 # Split the data based on the indices
 X_train = X[train_indices]
 y_train = y[train_indices]
+X_val = X[val_indices]
+y_val = y[val_indices]
 X_test = X[test_indices]
 y_test = y[test_indices]
 
 model = LSTM_model((look_back+1, num_features), 1)
 
-epochs = 20
-batch_size = 128
-
-history = model.train(X_train, y_train, X_test, y_test, epochs, batch_size)
-
-predictions = model.predict(X_test)
+history = model.train(X_train, y_train, epochs = 20, batch_size = 128, validation_data=(X_val, y_val))
 
 # Evaluate the model
 loss = model.evaluate(X_test, y_test)
 print("Mean, std deviation")
 print("%.2f%% (+/- %.2f%%)" % (np.mean(loss)*100, np.std(loss)*100))
 
+# Make predictions
+predictions = model.predict(X_test)
+
 # Plot history
-plt.clf()
-plt.plot(history.history['loss'], label='train_loss')
-plt.plot(history.history['val_loss'], label='val_loss')
+plt.plot(history.history['loss'], label='Training Loss')
+plt.plot(history.history['val_loss'], label='Validation Loss')
+plt.xlabel('Epoch')
+plt.ylabel('Loss')
 plt.legend()
+plt.show()
 
 directory = './'
 file = directory + str(epochs) + '_' + str(batch_size) + "_0.jpg"
