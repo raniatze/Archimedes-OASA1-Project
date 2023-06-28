1aa250c59a6807a453d5616b17d167cbf3d5e501
Alex Pap <alexpaa15@gmail.com>
2023-06-28 15:50:00 +0300

    final app

--- App/app.py
+++ App/app.py
@@ -2,20 +2,37 @@ from flask import Flask, render_template, request, redirect, url_for
 import pandas as pd
 import pymongo
 import folium
-import numpy as np
 import datetime
+import numpy as np
 
 app = Flask(__name__)
 
-ld = pd.read_csv('../Data/landmarks.csv')
-tax = pd.read_csv('../Data/staseis_taxi.csv',delimiter=";")
-met = pd.read_csv('../Data//staseis_metro.csv',delimiter=",")
+def get_output_string(strings, direction):
+    if direction == '1':
+        output = '-'.join(strings)
+    elif direction == '2':
+        output = '-'.join(strings[::-1])
+    else:
+        output = "Invalid direction value. Please choose 1 or 2."
+
+    return output
+
+def get_direction(df,name):
+    direction = np.where(df['Name'] == name)
+    direction = direction[0][0]
+    return df.iloc[direction,0]
+
+
+ld = pd.read_csv('../landmarks.csv')
+tax = pd.read_csv('../staseis_taxi.csv',delimiter=";")
+met = pd.read_csv('../staseis_metro.csv',delimiter=",")
 
 client = pymongo.MongoClient("mongodb://localhost:27017/")
 db = client['OASA1']
 collection = db['stops_by_line']
 collection1 = db['stops']
-predictions = db['ake']
+predictions = db['predictions']
+accessibility = db['staseis_proexoxes']
 choices = collection.distinct("Line_descr")
 
 selected_option = None
@@ -49,23 +66,36 @@ def dir():
     res2 = collection.find({"Line_descr": selected_option},{"Direction":1, "_id":0})
     res2 = list(res2)
     res2 = pd.DataFrame(res2)
+    print(res2)
     res2 = np.unique(res2)
-    if len(res2) < 2:
-        redirect(url_for('rinfo'))
-    return render_template('direction.html', selected_option = selected_option, choices = res2)
+    print(len(res2))
+    nam = selected_option
+    nam = nam.split("-")
+    nam = nam[1:]
+    global df
+    df = pd.DataFrame(columns=['Direction','Name'])
+    for i in range(len(res2)):
+        df.loc[i] = [res2[i],get_output_string(nam,res2[i])]
+    print(df)
+    #print(nam)
+    #if len(res2) < 2:
+    #    redirect(url_for('rinfo'))
+    return render_template('direction.html', selected_option = selected_option, choices = df['Name'])
 
 @app.route('/rinfo', methods=['POST'])
 def rinfo():
     global dir
-    dir = request.form.get('dropdown1')
+    dir1 = request.form.get('dropdown1')
+    dir = get_direction(df,dir1)
     print(dir)
-    print(selected_option)
-    results = collection.find({"Line_descr": str(selected_option), "Direction": int(dir)}, {"Stop_descr":1, "_id": 0})
+    #print(selected_option)
+    results = collection.find({"Line_descr": str(selected_option), "Direction": str(dir)}, {"Stop_descr":1, "_id": 0})
     data = list(results)
     stopss = pd.DataFrame(data)
     print(stopss)
+    #print(stopss)
     stops = stopss['Stop_descr'].tolist()
-    return render_template('routeinfo.html', selected_option=selected_option, stops=stops, dir = dir)
+    return render_template('routeinfo.html', selected_option=selected_option, stops=stops, dir = dir1)
 
 #@app.route('/rinfo', methods=['POST'])
 #def rinfoo():
@@ -79,7 +109,7 @@ def stopinfo():
     #print(selected_option)
     selected_stop = request.form.get('stop-dropdown')
     #print(selected_stop)
-    res = collection.find({"Line_descr":selected_option, "Stop_descr":str(selected_stop), "Direction": int(dir)}, {"Stop_lat":1,"Stop_lon":1,"_id":0})
+    res = collection.find({"Line_descr":selected_option, "Stop_descr":str(selected_stop), "Direction": str(dir)}, {"Stop_lat":1,"Stop_lon":1,"_id":0})
     res = list(res)
     print(res)
     res = pd.DataFrame(res)
@@ -89,6 +119,13 @@ def stopinfo():
     map = folium.Map(location=[latitude, longitude], zoom_start=zoom_level)
     folium.Marker(location=[latitude, longitude], popup=selected_stop,icon=folium.Icon(color = "red", icon="bus", prefix="fa")).add_to(map)
     #tooltip = "Click me!"
+    access = accessibility.find_one({"stop_descr":selected_stop},{"stop_descr":1,"_id":0})
+    if access:
+        access = True
+        print(access)
+    else : 
+        access = False
+        print(access)
     for i in range(len(ld)):
         marker = folium.Marker(
             location=[ld['latitude'][i], ld['longitude'][i]],
@@ -115,31 +152,44 @@ def stopinfo():
             #tooltip=tooltip
         )
         marker.add_to(map)
+    Day_of_year = '249'
+    year = '2021'
+    stop_encoding = collection.find({"Line_descr":selected_option, "Stop_descr":str(selected_stop), "Direction": str(dir)},{"Stop_encoding":1,"_id":0})
+    stop_encoding = list(stop_encoding)
+    stop_encoding = pd.DataFrame(stop_encoding)
+    print(stop_encoding.iloc[0,0])
 
-    # Get current datetime
+     # Get current datetime
     current_datetime = datetime.datetime.now()
 
     # Extract year, day of the year, and minute of the day
     year = current_datetime.year
     Day_of_year = current_datetime.timetuple().tm_yday
     minute_of_day = current_datetime.hour * 60 + current_datetime.minute
-    Day_of_year = '76'
-    year = '2023'
-    minute_of_day = 324
-    encoding = collection.find({"Line_descr":selected_option, "Stop_descr":str(selected_stop), "Direction": int(dir)},{"Stop_encoding":1,"Line_encoding":1,"_id":0})
+    print(Day_of_year)
+    print(minute_of_day)
+    print(selected_option)
+    #Day_of_year = '76'
+    #year = '2023'
+    #minute_of_day = 324
+    encoding = collection.find({"Line_descr":selected_option, "Stop_descr":str(selected_stop), "Direction": str(dir)},{"Stop_encoding":1,"Line_encoding":1,"_id":0})
     encoding = list(encoding)
     encoding = pd.DataFrame(encoding)
+    print(encoding)
 
-    res3 = predictions.find({"Line_descr":str(encoding.iloc[0,0]), "Stop_id":str(encoding.iloc[0,1]), "Direction": str(dir),"Day_of_year":Day_of_year,"Year":year}, {"Minute_of_day":1,"T_pa_in_veh":1,"_id":0})
+    res3 = predictions.find({"Line_descr":str(encoding.iloc[0,0]), "Stop_id":int(encoding.iloc[0,1]), "Direction": str(dir),"Day_of_year":Day_of_year}, {"Minute_of_day":1,"T_pa_in_veh":1,"_id":0})
     res3 = list(res3)
     print(res3)
     final_result = 0
     for d in res3:
         if int(d['Minute_of_day']) >= minute_of_day:
-            final_result = d['T_pa_in_veh']
+            final_result = round(d['T_pa_in_veh'])
             break
 
-    return render_template('stop.html', selected_option = selected_option,selected_stop=selected_stop, passengers=final_result, map=map._repr_html_())
+    #res3 = predictions.find({"Line_descr":selected_option, "Stop_id":str(stop_encoding.iloc[0,0]), "Direction": str(dir),"Day_of_year":Day_of_year,"Year":year}, {"Minute_of_day":1,"T_pa_in_veh":1,"_id":0})
+    #res3 = list(res3)
+    #print(res3)
+    return render_template('stop.html', selected_option = selected_option,selected_stop=selected_stop, final_result= final_result,access = access ,map=map._repr_html_())
 
 
 if __name__ == '__main__':
